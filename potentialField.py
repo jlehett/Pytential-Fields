827919ca83f82c0fb1aaaef72c5bf662b907c545
jlehett <jlehett117@knights.ucf.edu>
2019-12-02 00:03:25 -0500

    Add attraction and repulsion schemas.

--- potentialField.py
+++ potentialField.py
@@ -13,12 +13,15 @@ import math
 
 IN_DIRECTION = 0
 TO_TARGET = 1
+REPULSE = 2
+ATTRACT = 3
 
 
 """
     Aux Functions
 """
 
+@np.vectorize
 def cvtRange(x, in_min, in_max, out_min, out_max):
     """
         Convert a value, x, from its old range of
@@ -71,15 +74,28 @@ class PotentialField:
                 endPixelX = math.floor(startPixelX + fieldVector[0])
                 endPixelY = math.floor(startPixelY + fieldVector[1])
                 # Draw the vector to the pygame surface
-                pygame.draw.circle(
-                    surface, (255, 255, 255),
-                    (startPixelX, startPixelY), 
-                    math.floor(min(stride[0]/8.0, stride[1]/8.0))
-                )
+                
                 pygame.draw.line(
                     surface, (255, 255, 255),
                     (startPixelX, startPixelY), (endPixelX, endPixelY)
                 )
+    
+    def clampField(self, maxVel):
+        """
+            Clamp potential field such that the magnitude does not
+            exceed maxVel
+        """
+        magnitudeField = np.sqrt(
+            self.field[:, :, 0] ** 2 + self.field[:, :, 1] ** 2
+        )
+        normalField = np.zeros(
+            (self.fieldSize[0], self.fieldSize[1], 2)
+        )
+        normalField[:, :, 0] = self.field[:, :, 0] / magnitudeField
+        normalField[:, :, 1] = self.field[:, :, 1] / magnitudeField
+        magnitudeField = np.clip(magnitudeField, 0, maxVel)
+        self.field[:, :, 0] = normalField[:, :, 0] * magnitudeField
+        self.field[:, :, 1] = normalField[:, :, 1] * magnitudeField
 
     def addSchema(self, type, **kwargs):
         """
@@ -90,6 +106,10 @@ class PotentialField:
             self.field += self.inDirectionSchema(kwargs)
         if type == TO_TARGET:
             self.field += self.toTargetSchema(kwargs)
+        if type == REPULSE:
+            self.field += self.repulseSchema(kwargs)
+        if type == ATTRACT:
+            self.field += self.attractSchema(kwargs)
     
     def inDirectionSchema(self, kwargs):
         """
@@ -124,14 +144,14 @@ class PotentialField:
         y = np.linspace(0, self.fieldSize[1]-1, self.fieldSize[1])
         meshgrid = np.meshgrid(x, y, sparse=False, indexing='ij')
         # Find distance from coordinate to target
-        meshgridY = meshgrid[0] - targetPos[0]
-        meshgridX = meshgrid[1] - targetPos[1]
+        meshgridX = meshgrid[0] - targetPos[0]
+        meshgridY = meshgrid[1] - targetPos[1]
         # Create field out of these distance calculations
         field = np.zeros(
             (self.fieldSize[0], self.fieldSize[1], 2)
         )
-        field[:, :, 1] = meshgridX
-        field[:, :, 0] = meshgridY
+        field[:, :, 0] = meshgridX
+        field[:, :, 1] = meshgridY
         # Clamp the values of the field.
         magnitudeField = np.sqrt(
             field[:, :, 0] ** 2 + field[:, :, 1] ** 2
@@ -151,7 +171,107 @@ class PotentialField:
         field[:, :, 1] = normalField[:, :, 1] * -magnitudeField
         return field
         
+    def repulseSchema(self, kwargs):
+        """
+            Add schema that avoids a specified coordinate.
 
+            kwargs should contain:
+                repulsePos (float, float)
+                radius (float)
+                minVel (float)
+                maxVel (float)
+        """
+        repulsePos = kwargs['repulsePos']
+        # Create coordinate array to find distance
+        x = np.linspace(0, self.fieldSize[0]-1, self.fieldSize[0])
+        y = np.linspace(0, self.fieldSize[1]-1, self.fieldSize[1])
+        meshgrid = np.meshgrid(x, y, sparse=False, indexing='ij')
+        # Find distance from target to coordinate
+        meshgridX = meshgrid[0] - repulsePos[0]
+        meshgridY = meshgrid[1] - repulsePos[1]
+        # Create field out of these distance calculations
+        field = np.zeros(
+            (self.fieldSize[0], self.fieldSize[1], 2)
+        )
+        field[:, :, 0] = meshgridX
+        field[:, :, 1] = meshgridY
+        # Create magnitude field representing these distances
+        magnitudeField = np.sqrt(
+            field[:, :, 0] ** 2 + field[:, :, 1] ** 2
+        )
+        magnitudeField = np.clip(
+            magnitudeField, 0.0000001, math.inf
+        )
+        # Create normal field
+        normalField = np.zeros(
+            (self.fieldSize[0], self.fieldSize[1], 2)
+        )
+        normalField[:, :, 0] = field[:, :, 0] / magnitudeField
+        normalField[:, :, 1] = field[:, :, 1] / magnitudeField
+        # Adjust magnitude field to fit radius parameter
+        magnitudeField[
+            np.where(magnitudeField <= kwargs['radius'])
+        ] = cvtRange(magnitudeField[
+            np.where(magnitudeField <= kwargs['radius'])
+        ], 0, kwargs['radius'], kwargs['maxVel'], kwargs['minVel'])
+        magnitudeField[
+            np.where(magnitudeField > kwargs['radius'])
+        ] = 0
+        # Create final field
+        field[:, :, 0] = normalField[:, :, 0] * magnitudeField
+        field[:, :, 1] = normalField[:, :, 1] * magnitudeField
+        return field 
+
+    def attractSchema(self, kwargs):
+        """
+            Add schema that avoids a specified coordinate.
+
+            kwargs should contain:
+                attractPos (float, float)
+                radius (float)
+                minVel (float)
+                maxVel (float)
+        """
+        attractPos = kwargs['attractPos']
+        # Create coordinate array to find distance
+        x = np.linspace(0, self.fieldSize[0]-1, self.fieldSize[0])
+        y = np.linspace(0, self.fieldSize[1]-1, self.fieldSize[1])
+        meshgrid = np.meshgrid(x, y, sparse=False, indexing='ij')
+        # Find distance from target to coordinate
+        meshgridX = attractPos[0] - meshgrid[0]
+        meshgridY = attractPos[1] - meshgrid[1]
+        # Create field out of these distance calculations
+        field = np.zeros(
+            (self.fieldSize[0], self.fieldSize[1], 2)
+        )
+        field[:, :, 0] = meshgridX
+        field[:, :, 1] = meshgridY
+        # Create magnitude field representing these distances
+        magnitudeField = np.sqrt(
+            field[:, :, 0] ** 2 + field[:, :, 1] ** 2
+        )
+        magnitudeField = np.clip(
+            magnitudeField, 0.0000001, math.inf
+        )
+        # Create normal field
+        normalField = np.zeros(
+            (self.fieldSize[0], self.fieldSize[1], 2)
+        )
+        normalField[:, :, 0] = field[:, :, 0] / magnitudeField
+        normalField[:, :, 1] = field[:, :, 1] / magnitudeField
+        # Adjust magnitude field to fit radius parameter
+        magnitudeField[
+            np.where(magnitudeField <= kwargs['radius'])
+        ] = cvtRange(magnitudeField[
+            np.where(magnitudeField <= kwargs['radius'])
+        ], 0, kwargs['radius'], kwargs['maxVel'], kwargs['minVel'])
+        magnitudeField[
+            np.where(magnitudeField > kwargs['radius'])
+        ] = 0
+        # Create final field
+        field[:, :, 0] = normalField[:, :, 0] * magnitudeField
+        field[:, :, 1] = normalField[:, :, 1] * magnitudeField
+        return field 
 
 
 """
@@ -167,14 +287,23 @@ if __name__ == '__main__':
     pf = PotentialField(FIELD_SIZE)
 
     pf.addSchema(
-        TO_TARGET,
-        targetPos=(FIELD_SIZE[0]/4, FIELD_SIZE[1]/4), minVel=0, maxVel=10,
+        REPULSE,
+        repulsePos=(FIELD_SIZE[0]/2, FIELD_SIZE[1]/2), 
+        minVel=0, maxVel=10, radius=300
     )
+    
     pf.addSchema(
-        TO_TARGET,
-        targetPos=(FIELD_SIZE[0] * 3.0 / 4.0, FIELD_SIZE[1]*3.0/4.0),
-        minVel=0, maxVel=10
+        ATTRACT,
+        attractPos=(FIELD_SIZE[0]*3.0/4.0, FIELD_SIZE[1]/2), 
+        minVel=0, maxVel=10, radius=300
     )
+    
+    pf.addSchema(
+        IN_DIRECTION,
+        magnitude=10.0, angle=0.0
+    )
+
+    pf.clampField(10.0)
     
     while True:
         event = pygame.event.poll()
@@ -191,7 +320,7 @@ if __name__ == '__main__':
         pf.drawPotentialField(
             display.getScreen(),
             FIELD_SIZE,
-            stride=(30, 30)
+            stride=(20, 20)
         )
 
         display.updateScreen()
